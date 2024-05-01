05fa3359dbc15ef5c647aae559a9c09d26962928
jgtaeeun <167741370+jgtaeeun@users.noreply.github.com>
2024-05-01 23:12:29 +0900

    Update 08.js

--- 08/08.js
+++ 08/08.js
@@ -1,58 +1,43 @@
-document.addEventListener('DOMcontentLoaded', ()=>{
-
-   const bts = document.querySelectorAll('.bt7');
-   const txt1 =document.querySelector('#txt1');
-   const txt2 =document.querySelector('#txt2');
-  
+document.addEventListener('DOMContentLoaded',()=>{
+   const bts=document.querySelectorAll('.bt8');
+   const txt2=document.querySelector('#txt2');
+   const txt1=document.querySelector('#txt1');
+   
    bts[0].addEventListener('click', (e)=>{
-    console.log(text1.value)
-    e.preventDefault();
-       
+        e.preventDefault();
+       //방법1
+    //     let t1=[...txt1.value];
+    //     let t2=[];
         
-    //     let t1 =[...text1.value];
-    //     let t2 = [];
-       
-    //     for(let i = t1.length-1 ; i>=0 ; i--){
-    //      t2.push(t1[i]);
-    //     }
-    // // // 비교 :  배열
-    //     for(let i = 0 ; i<t1.length; i++){
-    //          if (t1[i]!=t2[i]){
-    //           text2.value="회문이 아닙니다.";
-    //           break;
-    //         }
-    //         else text2.value="회문입니다.";
-    //     }
-    // t1 = t1.join('');
-    // t2 = t2.join('');
-    // if (t1===t2) text2.value="회문입니다.";
-    // else text2.value="회문 아닙니다.";
+    //    for(let i=t1.length-1;i>=0;i--){
+    //     t2.push(t1[i]);
+    //    }
 
-    // //--------------------
-    // let a = text1.value;
-    // let b = text1.value.split('').reverse().join('');
-    // if (t1===t2) text2.value="회문입니다.";
-    // else text2.value="회문 아닙니다.";
+    //     t1=t1.join('');
+    //     t2=t2.join('');
+    //     if(t1===t2) txt2.value="회문입니다.";
+    //     else  txt2.value="회문이 아닙니다.";
+
+    //방법2
+       let a=txt1.value;
+       let b=txt1.value.split('').reverse().join('');
+       if(a===b) txt2.value="회문입니다.";
+       else  txt2.value="회문이 아닙니다.";
+
+   });
+   bts[1].addEventListener('click', (e)=>{
+
+    e.preventDefault();
+    let total=0;
+    for(let c of txt1.value){
+        if((c>=1)&&(c<=9)) total=total+parseInt(c);
+    }
+    txt2.value=total;
+
+   });
+
+   
 
 
-
-
-
-//    });
-//    bts[1].addEventListener('click', (e)=>{
-//     e.preventDefault();
-
-//     // for (let i = 0 ; i< text1.value.length ; i++){
-//     //     console.log(text1.value[i])
-//     // }
-//        let total = 0;
-//        for (let c of text1.value){
-//            if(c>='0' && c<=9){
-//             total=total +parseInt(c);
-//            }
-//        }
-//        text2.value=total;
-
-    });
 
 });
